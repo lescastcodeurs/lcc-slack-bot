dd709652fa3b5640989e7712ceb660b9300e060a
Marc Wrobel <marc.wrobel@gmail.com>
2022-06-11 17:12:12 +0200

    Implement bot skeletton
    Implementation is using Slack's Bolt framework for Java (https://api.slack.com/start/building/bolt-java) and connect to slack with socket mode (https://slack.dev/java-slack-sdk/guides/socket-mode).

--- README.md
+++ README.md
@@ -7,9 +7,25 @@ A slack bot that automates show notes creation for _Les Cast Codeurs podcast_.
 This bot is using the [Socket Mode](https://api.slack.com/apis/connections/socket) and can be set up
 without much hassle.
 
-First you need to create a [Slack app](https://api.slack.com/start) and install it :
+First you need to create a [Slack app](https://api.slack.com/start) and install it in your workspace :
 
 1. go to [https://api.slack.com/apps](https://api.slack.com/apps),
 2. create a new Slack app using this [manifest.yml](/src/main/slack/manifest.yml),
-3. optionally set an app icon,
-4. in the app basic information, install the app using the _Install to worspace_ button.
+3. optionally set an app icon in _Settings > Basic Information_,
+4. create an _App-Level Tokens_ with the scope `connections:write` in _Settings > Basic Information_ and save it for
+   later (it will be referred as `SLACK_APP_TOKEN`),
+5. install the app using the _Install to worspace_ button in _Settings > Basic Information_,
+6. save the _Bot User OAuth Token_ for later in _Features > OAuth & Permissions_ (it will be referred
+   as `SLACK_BOT_TOKEN`).
+
+You can then start the bot using the following commands (from the project directory) :
+
+```shell
+quarkus build
+
+export SLACK_APP_TOKEN='xapp-XXX'
+export SLACK_BOT_TOKEN='xoxb-XXX'
+java -jar build/quarkus-app/quarkus-run.jar
+```
+
+Once the bot is started, you can mention it in your messages : `@lcc help !`.
--- src/main/java/com/lescastcodeurs/bot/SlackBotRunner.java
+++ src/main/java/com/lescastcodeurs/bot/SlackBotRunner.java
@@ -1,21 +1,47 @@
 package com.lescastcodeurs.bot;
 
-import io.quarkus.runtime.Quarkus;
+import com.slack.api.bolt.App;
+import com.slack.api.bolt.AppConfig;
+import com.slack.api.bolt.socket_mode.SocketModeApp;
+import com.slack.api.model.event.AppMentionEvent;
 import io.quarkus.runtime.QuarkusApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 /**
  * Implements the bot logic.
  */
 public class SlackBotRunner implements QuarkusApplication {
 
+  public static final String SLACK_BOT_TOKEN_ENV_NAME = "SLACK_BOT_TOKEN";
+  public static final String SLACK_APP_TOKEN_ENV_NAME = "SLACK_APP_TOKEN";
   private static final Logger LOG = LoggerFactory.getLogger(SlackBotRunner.class);
 
   @Override
   public int run(String... args) {
-    LOG.info("Waiting for mentions from Slack...");
-    Quarkus.waitForExit();
-    return 0;
+    try {
+      createApp().start();
+      return 0;
+    } catch (Exception e) {
+      LOG.error("Could not create or start app");
+      return 1;
+    }
+  }
+
+  private SocketModeApp createApp() throws IOException {
+    String botToken = System.getenv(SLACK_BOT_TOKEN_ENV_NAME);
+    AppConfig appConfig = AppConfig.builder().singleTeamBotToken(botToken).build();
+
+    App app = new App(appConfig);
+    app.event(AppMentionEvent.class, (req, ctx) -> {
+      LOG.debug("Received : {}", req);
+      ctx.say("Hey there !");
+      return ctx.ack();
+    });
+
+    String appToken = System.getenv(SLACK_APP_TOKEN_ENV_NAME);
+    return new SocketModeApp(appToken, app);
   }
 }
